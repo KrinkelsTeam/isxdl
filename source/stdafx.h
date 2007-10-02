daae0fe0372b606e4c5c49bdbc708e09ee4c789f
bhenden <bhenden@81e47559-863b-0410-8d9b-d643a69c736b>
2007-10-02 19:45:51 +0000

    Changes needed for added files
    git-svn-id: https://svn.code.sf.net/p/istool/code/isxdl/trunk@26 81e47559-863b-0410-8d9b-d643a69c736b

--- source/stdafx.h
+++ source/stdafx.h
@@ -22,7 +22,7 @@ extern CAppModule _Module;
 #include <atlutil.h>
 #include <atlfile.h>
 #include <atltypes.h>
-#include <henden/all.h>
+#include "Thread.h"
 
 // --------------------------------------------------------- Internet
 #include <wininet.h>
